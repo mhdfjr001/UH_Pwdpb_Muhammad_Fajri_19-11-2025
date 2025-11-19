52ffeeb6a9fce0905736f922eb0f95e74a37a22a
mhdfjr001 <muhammadfajri2400@gmail.com>
2025-11-19 14:06:59 +0700

    Update delete.php to remove from tbl_dosen

--- UH_PWDBP_Muhammad_Fajri_XIPPLG1/delete.php
+++ UH_PWDBP_Muhammad_Fajri_XIPPLG1/delete.php
@@ -2,6 +2,7 @@
 include "koneksi.php";
 $id = $_GET['id'];
 
-mysqli_query($conn, "DELETE FROM tbl_fajri WHERE nidn='$id'");
+mysqli_query($conn, "DELETE FROM tbl_dosen WHERE nidn='$id'");
 header("Location: index.php");
 ?>
+
